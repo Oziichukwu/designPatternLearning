64ca8c21eb52b90d5bf4fadcecf5ee5914636d6e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 00:13:07 +0100

    patterns

--- src/designPatterns/strategyPattern/strategyPatternUsingCharacters/FightTimeMain.java
+++ src/designPatterns/strategyPattern/strategyPatternUsingCharacters/FightTimeMain.java
@@ -1,2 +1,21 @@
-package designPatterns.strategyPattern.strategyPatternUsingCharacters;public class FightTimeMain {
+package designPatterns.strategyPattern.strategyPatternUsingCharacters;
+
+public class FightTimeMain {
+
+    public static void main(String[] args) {
+
+        Character segun = new Knight();
+        Character williams = new Troll();
+
+        String willy =williams.performfight();
+        System.out.println("Troll has this to say: " + willy);
+
+        System.out.println("Knight has this to say: " + segun.performfight());
+
+        Character billy = new King();
+        System.out.println("King has this to say: " + billy.performfight());
+
+        billy.setMyWeapons(new SwordBehaviour());
+        System.out.println("King has change his weapons:" + billy.performfight());
+    }
 }
