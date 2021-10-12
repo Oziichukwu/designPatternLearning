8e003788567b2da80d2df6f6c02c8014db742e58
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 00:10:52 +0100

    patterns

--- src/designPatterns/strategyPattern/strategyPatternUsingCharacters/WeaponBehaviour.java
+++ src/designPatterns/strategyPattern/strategyPatternUsingCharacters/WeaponBehaviour.java
@@ -1,2 +1,38 @@
-package designPatterns.strategyPattern.strategyPatternUsingCharacters;public interface Fight {
+package designPatterns.strategyPattern.strategyPatternUsingCharacters;
+
+public interface WeaponBehaviour {
+
+    String fight();
+}
+
+class SwordBehaviour implements WeaponBehaviour{
+
+    @Override
+    public String fight() {
+        return "I can fight with a sword perfectly";
+    }
+}
+
+class BowAndArrowBehaviour implements WeaponBehaviour{
+
+    @Override
+    public String fight() {
+        return "The only weapon i can use very well is a bow and arrow";
+    }
+}
+
+class KnifeBehaviour implements WeaponBehaviour{
+
+    @Override
+    public String fight() {
+        return "you can only die by my hands with a knife";
+    }
+}
+
+class AxeBehaviour implements WeaponBehaviour{
+
+    @Override
+    public String fight() {
+        return "Axe is the surest way to kill";
+    }
 }
