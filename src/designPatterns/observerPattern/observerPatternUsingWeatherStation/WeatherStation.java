cb14ce54a7372cb516739f628bf6c280057e314b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 00:14:23 +0100

    strategy pattern

--- src/designPatterns/observerPattern/observerPatternUsingWeatherStation/WeatherStation.java
+++ src/designPatterns/observerPattern/observerPatternUsingWeatherStation/WeatherStation.java
@@ -1,2 +1,17 @@
-package designPatterns.observerPattern.observerPatternUsingWeatherStation;public class WeatherStation {
+package designPatterns.observerPattern.observerPatternUsingWeatherStation;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        weatherData.setMeasurements(80,65,30.4f);
+        weatherData.setMeasurements(80,70,29.2f);
+        weatherData.setMeasurements(78,90,29.2f);
 }
+        }
--- src/designPatterns/strategyPattern/strategyPatternUsingCharacters/Character.java
+++ src/designPatterns/strategyPattern/strategyPatternUsingCharacters/Character.java
@@ -1,5 +1,16 @@
 package designPatterns.strategyPattern.strategyPatternUsingCharacters;
 
-public class King {
+public class Character {
 
+    WeaponBehaviour myWeapons;
+
+
+    public String performfight(){
+
+        return myWeapons.fight();
+    }
+
+    public void setMyWeapons(WeaponBehaviour newWeaponBehaviour){
+        myWeapons = newWeaponBehaviour;
+    }
 }
