886b660312253de761f685b5e99bfc6b30b80222
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 00:15:39 +0100

    patterns

--- src/designPatterns/observerPattern/observerPatternUsingWeatherStation/WeatherData.java
+++ src/designPatterns/observerPattern/observerPatternUsingWeatherStation/WeatherData.java
@@ -1,2 +1,43 @@
-package designPatterns.observerPattern.observerPatternUsingWeatherStation;public class WeatherData {
+package designPatterns.observerPattern.observerPatternUsingWeatherStation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject{
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData(){
+        observers = new ArrayList<Observer>();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+            observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObserver() {
+        for (Observer observer: observers){
+            observer.update(temperature,humidity,pressure);
+        }
+    }
+
+    public void measurementChanged(){
+        notifyObserver();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this. humidity = humidity;
+        this.pressure = pressure;
+        measurementChanged();
+    }
 }
