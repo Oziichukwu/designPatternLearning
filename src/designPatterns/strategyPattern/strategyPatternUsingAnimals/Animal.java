5ca063ee0cce70badaccec5434bbc53bb673e3e0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-28 01:40:11 +0100

    design pattern

--- src/designPatterns/strategyPattern/strategyPatternUsingAnimals/Animal.java
+++ src/designPatterns/strategyPattern/strategyPatternUsingAnimals/Animal.java
@@ -1,2 +1,15 @@
-package designPatterns.strategyPattern.strategyPatternUsingAnimals;public class Animal {
+package designPatterns.strategyPattern.strategyPatternUsingAnimals;
+
+public class Animal {
+    Movable movingType;
+
+    public String performMoving(){
+
+        return movingType.move();
+    }
+
+    public void setMovingAbility(Movable movingType){
+
+        this.movingType = movingType;
+    }
 }
--- src/designPatterns/strategyPattern/strategyPatternUsingAnimals/AnimalMain.java
+++ src/designPatterns/strategyPattern/strategyPatternUsingAnimals/AnimalMain.java
@@ -1,2 +1,24 @@
-package designPatterns.strategyPattern.strategyPatternUsingAnimals;public class AnimalMain {
+package designPatterns.strategyPattern.strategyPatternUsingAnimals;
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+
+        Animal buddy = new Dog();
+
+        Animal sweet = new Bird();
+
+        Animal rankko = new Fish();
+
+        System.out.println("Dog " + buddy.performMoving());
+        System.out.println("Bird " + sweet.performMoving());
+        System.out.println("Fish " + rankko.performMoving());
+
+        Animal nancho = new Fish();
+        nancho.setMovingAbility(new MovesByFlying());
+        System.out.println("nancho the fish has this this to say: " + nancho.performMoving());
+
+        Animal crikky = new Cricket();
+        System.out.println("Cricket " + crikky.performMoving());
+    }
 }
